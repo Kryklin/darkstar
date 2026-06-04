4aa2fe8b82312f9d98aeeff58ea019e4978cd9cc
kryklin <mortalpain1@gmail.com>
2026-06-04 01:36:16 +0100

    feat: implement CUDA CLI and WASM stubs for dasp-crypto operations

--- cuda/dasp_cli.cu
+++ cuda/dasp_cli.cu
@@ -258,8 +258,7 @@ int main(int argc, char **argv) {
         for(int i=0; i<32; i++) sprintf(&mac_in_hex_diagnostic[i*2], "%02x", mac_in[i]);
         for(int i=0; i<32; i++) sprintf(&mac_actual_hex[i*2], "%02x", actual_mac[i]);
         
-        fprintf(stderr, "{\"diagnostics\":{\"stage1_blended_ss\":\"%s\",\"stage2_word_key\":\"%s\",\"stage4_mac_in\":\"%s\",\"stage4_mac\":\"%s\"}}\n", 
-               blended_hex, word_key_hex, mac_in_hex_diagnostic, mac_actual_hex);
+
         
         if (use_telemetry) {
             fprintf(stderr, "{\"timings\":{\"cascade_us\":%llu}}\n", cascade_us);
@@ -455,8 +454,7 @@ int main(int argc, char **argv) {
         for(int i=0; i<32; i++) sprintf(&blended_hex[i*2], "%02x", blended_ss[i]);
         // [word_key_hex already defined above]
         
-        fprintf(stderr, "{\"diagnostics\":{\"stage1_blended_ss\":\"%s\",\"stage2_word_key\":\"%s\",\"stage4_mac_in\":\"%s\",\"stage4_mac\":\"%s\"}}\n", 
-               blended_hex, word_key_hex, mac_hex, mac_hex);
+
         
         if (use_telemetry) {
             fprintf(stderr, "{\"timings\":{\"cascade_us\":%llu}}\n", cascade_us);
